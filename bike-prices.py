4dec0f41cbb9816fa7ffefcb96b6a9996c5e201a
NicolayB <ne.barrera@uniandes.edu.co>
2024-08-14 16:45:01 -0500

    Impresion de los datos buscados

--- bike-prices.py
+++ bike-prices.py
@@ -1,33 +1,32 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("BikePrices.csv")
-df.head()
+print(df.head())
+print("\n", df.shape)
 
-df.shape
+print("\n", df["Brand"].unique())
 
-df["Brand"].unique()
+print("\n", df.groupby("Brand").count())
 
-df.groupby("Brand").count()
+print("\n", df["Brand"].value_counts())
 
-df["Brand"].value_counts()
-
-df.describe()
+print("\n", df.describe())
 
 # Porcentaje de motos marca Honda
 count = df["Brand"].value_counts()
 perc_honda = (count.loc["Honda"]/count.sum())*100
-print(f"Honda Rate = {perc_honda:.2f}%")
+print("\n", f"Honda Rate = {perc_honda:.2f}%")
 
 # Número de duplicados
 duplicates = len(df[df.duplicated()])
-print(f'Number of Duplicate Entries: {duplicates}')
+print("\n", f'Number of Duplicate Entries: {duplicates}')
 
 # Número de valores perdidos
 missing_values = df.isnull().sum().sum()
-print(f'Number of Missing Values: {missing_values}')
+print("\n", f'Number of Missing Values: {missing_values}')
 
+print("\n")
 # Tipos de datos en el dataset
 types = df.dtypes.value_counts()
 print('Number of Features: %d'%(df.shape[1]))
